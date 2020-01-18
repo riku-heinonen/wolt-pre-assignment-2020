54cb79dd817665fbffe0af034b176eaf7f233b2d
Riku Heinonen <riku.s.heinonen@aalto.fi>
2020-01-18 14:16:00 +0200

    Add recalculating of blurhashes
    Also update the requirements.txt to have all the needed modules for blurhashing

--- restaurant_API.py
+++ restaurant_API.py
@@ -1,24 +1,48 @@
 from flask import Flask, jsonify, request, json, Response
 import geopy.distance
+from PIL import Image, ImageOps
+import numpy as np
+import blurhash
+import requests
+import io
 
 app = Flask(__name__)
 
-# initialise the database
+
+def calculate_hash(image_url):
+    ''' Calculate the blurhash for an url containing an image '''
+    image_response = requests.get(image_url, stream=True)
+    image_content = image_response.content
+    image = Image.open(io.BytesIO(image_content))
+    # downscaling the image yields much better performance 
+    # with little to no quality loss as the image is blurred anyway
+    size = (20, 20)
+    downscaled = ImageOps.fit(image, size, Image.NEAREST)
+    pixels = np.array(downscaled.convert("RGB"))
+    hash = blurhash.encode(pixels)
+    return hash
+
+
+# initialise the dataset
 with open('restaurants.json', 'r') as file:
     restaurant_dict = json.load(file)
+    for restaurant in restaurant_dict['restaurants']:
+        # recalculate the invalid blurhashes in the dataset
+        new_hash = calculate_hash(restaurant['image'])
+        restaurant['blurhash'] = new_hash
 
 
 @app.route('/restaurants/', methods=['GET'])
 def get_restaurants():
-    """ return the data of all restaurants in the database """
+    ''' Return the data of all restaurants in the database '''
     return jsonify(restaurant_dict['restaurants'])
 
 
 @app.route('/restaurants/search/', methods=['GET'])
 def get_restaurant():
-    """ Return the data of all restaurants that include the queried tag in their
+    ''' Return the data of all restaurants that include the queried tag in their
         tags and are less than 3 kilometers away from the queried location.
-    """
+    '''
     try:
         tag = request.args['q']
         lat = float(request.args['lat'])
@@ -45,4 +69,4 @@ def get_restaurant():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
