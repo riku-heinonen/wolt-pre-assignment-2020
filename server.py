fa8827b71eb8fbd3ff885642380caba400e397c9
Riku Heinonen <riku.s.heinonen@aalto.fi>
2020-01-27 15:40:44 +0200

    Format the code using YAPF

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 from flask import Flask, jsonify, request, json, Response
-import geopy.distance
 from PIL import Image, ImageOps
+import geopy.distance
 import numpy as np
 import blurhash
 import requests
 import io
-
 
 # Config
 
@@ -15,8 +14,9 @@ RECALCULATE_HASHES = False
 
 app = Flask(__name__)
 
+
 def calculate_hash(image_url):
-    ''' Calculate the blurhash of an image '''
+    """ Calculate the blurhash of an image """
     image_response = requests.get(image_url, stream=True)
     image_content = image_response.content
     image = Image.open(io.BytesIO(image_content))
@@ -31,62 +31,72 @@ def calculate_hash(image_url):
 
 
 # initialise the dataset
-with open('restaurants.json', 'r') as file:
-    restaurants = json.load(file)['restaurants']
+with open("restaurants.json", "r") as file:
+    restaurants = json.load(file)["restaurants"]
     for restaurant in restaurants:
         # make the location attribute a dictionary
         # to avoid confusion later on
-        location = {'lat': restaurant['location'][1],
-                    'lon: restaurant['location'][0]}
-        restaurant['location'] = location
+        location = {
+            "lat": restaurant["location"][1],
+            "lon": restaurant["location"][0]
+        }
+        restaurant["location"] = location
         if RECALCULATE_HASHES:
             # recalculate the invalid blurhashes in the dataset
-            new_hash = calculate_hash(restaurant['image'])
-            restaurant['blurhash'] = new_hash
+            new_hash = calculate_hash(restaurant["image"])
+            restaurant["blurhash"] = new_hash
 
 
-@app.route('/restaurants/', methods=['GET'])
+@app.route("/restaurants/", methods=["GET"])
 def get_restaurants():
-    ''' 
+    """ 
         Return HTTP 200 along with the data
         of all restaurants in the dataset. 
-    '''
+    """
     return jsonify(restaurants)
 
 
-@app.route('/restaurants/search/', methods=['GET'])
+@app.route("/restaurants/search/", methods=["GET"])
 def get_restaurant():
-    ''' 
+    """ 
         Fetch all restaurants that include the queried tag in their
         name, description or tags and are less than 3 kilometers away
         from the queried location. Return HTTP 200 along with the queried
         data or HTTP 400 if the query parameters were invalid. 
-    '''
+    """
     try:
-        tag = request.args['q']
-        lat = float(request.args['lat'])
-        lon = float(request.args['lon'])
+        tag = request.args["q"]
+        lat = float(request.args["lat"])
+        lon = float(request.args["lon"])
 
     # missing or invalid parameters
     except (KeyError, ValueError):
-        return Response(json.dumps({'error': 'Invalid query parameters.'}),
-                        status=400,
-                        mimetype="application/json")
+        return Response(
+            json.dumps({"error": "Invalid query parameters."}),
+            status=400,
+            mimetype="application/json",
+        )
     else:
         queried_location = (lat, lon)
         matching_restaurants = []
         for restaurant in restaurants:
-            if any(tag in field for field in [restaurant['description'],
-                                              restaurant['name'],
-                                              restaurant['tags']]):
-                current_location = (restaurant['location']['lat'], restaurant['location']['lon'])                             
+            tag_fields = [
+                restaurant["description"],
+                restaurant["name"],
+                restaurant["tags"],
+            ]
+            if any(tag in field for field in tag_fields):
+                current_location = (
+                    restaurant["location"]["lat"],
+                    restaurant["location"]["lon"],
+                )
                 distance = geopy.distance.distance(queried_location,
-                                                   current_location).m
-                if distance < 3000:
+                                                   current_location)
+                if distance.m < 3000:
                     matching_restaurants.append(restaurant)
 
         return jsonify(matching_restaurants)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- test_server.py
+++ test_server.py
@@ -8,60 +8,60 @@ from pprint import pprint
 
 @pytest.fixture
 def tags():
-    '''
+    """
         Returns all the possible tags in the dataset
-    '''
+    """
     tags = []
     for restaurant in restaurants:
-        tags.extend(restaurant['tags'])
+        tags.extend(restaurant["tags"])
     return list(set(tags))
 
 
 @pytest.fixture
 def coordinates():
-    '''
+    """
         Returns the corner coordinates of the bounding
         rectangle of the restaurants in the dataset
-    '''
+    """
     latitudes = []
     longitudes = []
     for restaurant in restaurants:
-        longitudes.append(restaurant['location']['lon'])
-        latitudes.append(restaurant['location']['lat'])
+        longitudes.append(restaurant["location"]["lon"])
+        latitudes.append(restaurant["location"]["lat"])
     return {
-        'min_lat': min(latitudes),
-        'max_lat': max(latitudes),
-        'min_lon': min(longitudes),
-        'max_lon': max(longitudes)
+        "min_lat": min(latitudes),
+        "max_lat": max(latitudes),
+        "min_lon": min(longitudes),
+        "max_lon": max(longitudes)
     }
 
 
 def test_get_restaurants():
-    '''
+    """
         Test that querying for all restaurants
         returns the correct amount of restaurants
-    '''
-    response = app.test_client().get('/restaurants/')
+    """
+    response = app.test_client().get("/restaurants/")
     response_list = json.loads(response.data)
     assert response.status_code == 200
     assert len(response_list) == 50
 
 
 def test_random_queries(tags, coordinates):
-    '''
-        Make 1000 queries with random parameters chosen from all of the possible tags
-        and random locations inside the bounding rectangle of the restaurants in the dataset.
-        If the query matches any restaurants, check that the location is less than 3000 metres away
-        from the queried location and the tags are included in the restaurant's name, description or tags.
-    '''
+    """
+        Make 1000 queries with random parameters chosen from all of the possible
+        tags and random locations inside the bounding rectangle of the
+        restaurants in the dataset. If the query matches any restaurants, check
+        that the location is less than 3000 metres away from the queried
+        location and the tags are included in the restaurant's name, description
+        or tags.
+    """
     for i in range(1000):
-        lon = random.uniform(coordinates['min_lon'],
-                             coordinates['max_lon'])
-        lat = random.uniform(coordinates['min_lat'],
-                             coordinates['max_lat'])
+        lon = random.uniform(coordinates["min_lon"], coordinates["max_lon"])
+        lat = random.uniform(coordinates["min_lat"], coordinates["max_lat"])
         queried_location = (lat, lon)
         tag = random.choice(tags)
-        url = f'/restaurants/search/?q={tag}&lat={lat}&lon={lon}'
+        url = f"/restaurants/search/?q={tag}&lat={lat}&lon={lon}"
         response = app.test_client().get(url)
 
         # the endpoint always returns HTTP 200 if the parameters
@@ -70,24 +70,25 @@ def test_random_queries(tags, coordinates):
         if response.data:
             response_list = json.loads(response.data)
             for restaurant in response_list:
-                tag_fields = [restaurant['name'],
-                              restaurant['description'],
-                              restaurant['tags']]
+                tag_fields = [
+                    restaurant["name"], restaurant["description"],
+                    restaurant["tags"]
+                ]
                 assert any(tag in field for field in tag_fields)
 
-                current_location = (restaurant['location']['lat'],
-                                    restaurant['location']['lon'])
+                current_location = (restaurant["location"]["lat"],
+                                    restaurant["location"]["lon"])
                 distance = geopy.distance.distance(queried_location,
                                                    current_location)
                 assert distance.m < 3000
 
 
 def test_invalid_query():
-    '''
+    """
         Test that giving invalid query parameters returns HTTP 400 bad request
-    '''
+    """
     response = app.test_client().get(
-        '/restaurants/search/?q=sushi&lat=latitude&lon=longitude')
+        "/restaurants/search/?q=sushi&lat=latitude&lon=longitude")
     response_data = json.loads(response.data)
     assert response.status_code == 400
-    assert response_data['error'] == 'Invalid query parameters.'
+    assert response_data["error"] == "Invalid query parameters."
